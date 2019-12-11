3141565eec1b1b54ae0125abe5cc136d6ed563c4
Hee Seok Shin <4sizn@naver.com>
2019-12-11 23:31:27 +0900

    feat : add rps_library

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const rps_game = require("rps-game");
+const { HAND, RESULT, game, getGameResult, makePlayer } = rps_game;
 console.log("hello world");
 const gameEl = document.querySelector(".game");
+
+//will update in library
+const makeComputers = n => {
+  const players = [];
+  for (let i = 0; i < n; i++) {
+    players.push(makePlayer(Math.floor(Math.random() * 3)));
+  }
+  return players;
+};
+
+function gameStart() {
+  console.log(game([makeComputers(1), makeComputers(1)]));
+}
+
+gameStart();
