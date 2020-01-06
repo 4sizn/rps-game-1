9b6c8c71c32e1388b61c876801bba3f1109e3d7d
Hee Seok Shin <4sizn@naver.com>
2020-01-06 09:46:30 +0900

    feat : check logic

--- index.js
+++ index.js
@@ -18,14 +18,50 @@ function gameStart() {
 
 gameStart();
 
-class Machine {
+class Commnad {
   constructor() {}
-  idle() {}
-  play() {
-    console.log("play");
-  }
-  end() {}
 }
 
-const _machine = new Machine();
-_machine.play();
+class ICommandBehavior {
+  execute() {
+    console.log("구현하세요");
+  }
+}
+
+class Game {
+  constructor() {
+    this._isPlay = false;
+  }
+
+  play() {
+    this._isPlay = true;
+  }
+
+  pause() {
+    this._isPlay = false;
+  }
+}
+
+//command Pattern
+class Machine {
+  //게임 레트로 게임팩 고증화
+  constructor(g) {
+    this._game = g;
+    this._coins = 0;
+    this._isPower = false;
+  }
+
+  powerOn() {
+    console.log("Machine, powerOn");
+    this._isPower = true;
+    this._game.play();
+  }
+  powerOff() {
+    console.log("Machine, powerOFF");
+    this._isPower = false;
+  }
+}
+
+const _machine = new Machine(new Game());
+_machine.powerOn();
+_machine.powerOff();
