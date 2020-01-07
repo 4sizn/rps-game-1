d536853e60ed6d972c3cbeabd29a95bd1c8c4928
Hee Seok Shin <4sizn@naver.com>
2020-01-07 09:31:23 +0900

    wip

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const rps_game = require("rps-game");
 const { HAND, RESULT, game, getGameResult, makePlayer } = rps_game;
-console.log("hello world");
-const gameEl = document.querySelector(".game");
+import { coroutine, defer } from "./helper";
 
 //will update in library
 const makeComputers = n => {
@@ -16,6 +15,14 @@ function gameStart() {
   console.log(game([makeComputers(1), makeComputers(1)]));
 }
 
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
 gameStart();
 
 class Commnad {
@@ -28,13 +35,45 @@ class ICommandBehavior {
   }
 }
 
+function controller() {}
+
 class Game {
-  constructor() {
+  constructor({ name }) {
     this._isPlay = false;
+    this._name = name;
+    this.start = this.start.bind(this);
+    this.isPending = this.isPending.bind(this);
+    this.loop = this.loop.bind(this);
   }
 
-  play() {
-    this._isPlay = true;
+  isPending() {
+    return this.userEvents.length;
+  }
+
+  loop() {
+    console.log("loop");
+    const { isPending, userEvents } = this;
+    return function* loop() {
+      while (this._isPlay) {
+        yield delay(1000).then(() => {
+          console.log("abcd");
+        });
+      }
+    }.bind(this);
+  }
+
+  start() {
+    const { loop } = this;
+    const that = this;
+    coroutine(
+      function*() {
+        yield delay(1000).then(() => {
+          console.log("started()", this);
+          that._isPlay = true;
+          coroutine(loop());
+        });
+      }.bind(this)
+    );
   }
 
   pause() {
@@ -42,6 +81,19 @@ class Game {
   }
 }
 
+//not use Class shape, use function composition to make flexible interface in js
+function RpsCreator() {
+  const _state = { name: "rps" };
+  const game = new Game(_state);
+  return {
+    ...game,
+    // ...rps(state)
+    idle: function*() {},
+    ...makeAnimation()
+  };
+}
+
+function makeAnimation() {}
 //command Pattern
 class Machine {
   //게임 레트로 게임팩 고증화
@@ -54,14 +106,17 @@ class Machine {
   powerOn() {
     console.log("Machine, powerOn");
     this._isPower = true;
-    this._game.play();
+    this._game.start();
   }
+
   powerOff() {
     console.log("Machine, powerOFF");
     this._isPower = false;
   }
 }
 
-const _machine = new Machine(new Game());
+const _machine = new Machine(RpsCreator());
 _machine.powerOn();
 _machine.powerOff();
+
+const gameEl = document.querySelector(".game");
